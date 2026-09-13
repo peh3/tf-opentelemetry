c1e7f58f0ef851421d31271505258c296dab302d
peh3 <tk_peh@hotmail.com>
2026-09-13 18:34:32 +0800

    updated

--- outputs.tf
+++ outputs.tf
@@ -12,7 +12,10 @@ output "demo_app_order_endpoint" {
   value       = "http://${aws_instance.app.public_ip}:8000/order"
   description = "Hit this endpoint to generate traces, metrics, and logs"
 }
-
+output "demo_app_firing" {
+  value       = "./traffic-generator.sh http://${aws_instance.app.public_ip}:8000"
+  description = "Hit this endpoint to generate traffic to the demo app (for testing and demo purposes)"
+}
 output "loki_internal_endpoint" {
   value       = "http://${aws_instance.loki.private_ip}:3100"
   description = "Private OTLP HTTP target for Loki"
